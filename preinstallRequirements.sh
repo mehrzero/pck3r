07c275393aba0c580ac514bad8e62f56aa5523ca
amzy-0 <azymiamin@gmail.com>
2020-11-16 20:46:02 +0330

    bug fixed => library of vte updated

--- preinstallRequirements.sh
+++ preinstallRequirements.sh
@@ -1,6 +1,6 @@
 sudo apt install gcc
 sudo apt install build-essential
 sudo apt install libgtk-3-dev
-sudo apt install libvte-2.91-0
+sudo apt install libvte-2.91-dev 
 mkdir -p ~/.pck3r-GUI/
 sudo cp -r icon/pck3r-logo.png  ~/.pck3r-GUI/pck3r-logo.png
