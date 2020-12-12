69c3aa80e4d50b31d12867c352e2702d6fb9e944
amzy-0 <azymiamin@gmail.com>
2020-12-12 23:22:59 +0330

    *libvte* package name  fixed

--- preinstallRequirements.sh
+++ preinstallRequirements.sh
@@ -1,6 +1,6 @@
-sudo apt install gcc
-sudo apt install build-essential
-sudo apt install libgtk-3-dev
-sudo apt install libvte-2.91-dev 
+sudo apt install gcc -y 
+sudo apt install build-essential -y
+sudo apt install libgtk-3-dev -y
+sudo apt install libvte-2.91-dev -y
 mkdir -p ~/.pck3r-GUI/
 sudo cp -r icon/pck3r-logo.png  ~/.pck3r-GUI/pck3r-logo.png
