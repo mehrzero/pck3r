5275710f6047db82c1f123172febcb45eabba29e
amzy-0 <azymiamin@gmail.com>
2021-02-17 13:49:05 +0330

    the installer button background changed to the blue

--- installer.py
+++ installer.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
-__authors__ = ["M.Amin Azimi .K (amzy-0)", "mehrzero", "https://github.com/amzy-0/pck3r/graphs/contributors"]
+__authors__ = ['M.Amin Azimi .K (amzy-0)', 'mehrzero', 'https://github.com/amzy-0/pck3r/graphs/contributors']
 
 
 from libs import stuff
@@ -23,86 +23,84 @@ from os import  getcwd, getenv, chdir
 from os import system as syscall
 ###############################################################################
 # preinstall requirements                                                     #
-syscall("mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon")#
-syscall("sudo apt install python3-tk -y")                                     #
-syscall("sudo apt install python3-pil python3-pil.imagetk -y")                #
-syscall("sudo apt install g++ -y")                                            # 
+syscall('mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon')#
+syscall('sudo apt install python3-tk -y')                                     #
+syscall('sudo apt install python3-pil python3-pil.imagetk -y')                #
+syscall('sudo apt install g++ -y')                                            # 
 ###############################################################################
-from tkinter import *
+import tkinter as tk
 from tkinter.ttk import *
 import time
 from PIL import ImageTk,Image
 
-
-
 # creating tkinter window
-root = Tk()
+root = tk.Tk()
 
 
-make_link = "sudo ln -s %s/.pck3r/core_pck3r.py /bin/pck3r" % getenv("HOME")
+make_link = 'sudo ln -s %s/.pck3r/core_pck3r.py /bin/pck3r' % getenv('HOME')
 
 # Progress bar widget
-progress = Progressbar(root, orient = HORIZONTAL,
-length = 100, mode = "determinate")
+progress = Progressbar(root, length = 100, mode = 'determinate')
 
 # Function responsible for the updation
 # of the progress bar value
 def bar():
 
-
-    progress["value"] = 20
+    # 20% PROGRESS
+    progress['value'] = 20
     root.update_idletasks()
     time.sleep(1)
-    print("Unlinking pck3r (if was installed) ")
-    syscall("sudo unlink /bin/pck3r ")
-    syscall("sudo rm -rf /bin/pck3r*")
-    progress["value"] = 40
+    print('Unlinking pck3r (if was installed) ')
+    syscall('sudo unlink /bin/pck3r ')
+    syscall('sudo rm -rf /bin/pck3r*')
+
+    # 40% PROGRESS
+    progress['value'] = 40
     root.update_idletasks()
     time.sleep(1)
+   
+    syscall('rm -rf ~/.pck3r') if (syscall('echo %s ; ls ~/.pck3r' % stuff.CYN)) ==0 else print('%sCopy all pck3r directory %s' %(stuff.CYN, stuff.NRM))
+    syscall('mkdir ~/.pck3r')
+    syscall('cp -rf . ~/.pck3r')
+    syscall('cp -rf . ~/.pck3r')
 
-
-
-
-    
-    syscall("rm -rf ~/.pck3r") if (syscall("echo %s ; ls ~/.pck3r" % stuff.CYN)) ==0 else print("%sCopy all pck3r directory %s" %(stuff.CYN, stuff.NRM))
-    syscall("mkdir ~/.pck3r")
-    syscall("cp -rf . ~/.pck3r")
-    progress["value"] = 50
+    # 50% PROGRESS
+    progress['value'] = 50
     root.update_idletasks()
     time.sleep(1)
-
     syscall(make_link)
-    progress["value"] = 60
+
+    # 60% PROGRESS
+    progress['value'] = 60
+    root.update_idletasks()
+    time.sleep(1)
+    print('%sCheck link ' % stuff.YEL)
+    print('Link created ') if (syscall('ls -l  /bin/pck3r')) == 0  else print('%s%sNo link (/bin/pck3r)%s ' % (stuff.sysOk(), stuff.RED, stuff.NRM))
+    progress['value'] = 80
     root.update_idletasks()
     time.sleep(1)
 
-    print("%sCheck link " % stuff.YEL)
-    print("Link created ") if (syscall("ls -l  /bin/pck3r")) == 0  else print("%s%sNo link (/bin/pck3r)%s " % (stuff.sysOk(), stuff.RED, stuff.NRM))
-    progress["value"] = 80
-    root.update_idletasks()
-    time.sleep(1)
-    progress["value"] = 100
-
-    print("%s%sPck3r installed successfuly %s" % (stuff.sysOk(), stuff.GRN, stuff.NRM))
-    # user theme and window configuration
+    # 100% PROGRESS
+    progress['value'] = 100
+    print('%s%sPck3r installed successfuly %s' % (stuff.sysOk(), stuff.GRN, stuff.NRM))
+    
     root.quit()
 
 
 # packing to main window (panel)
-icon = Image.open("%s/.pck3r/icon/pck3r-logo.png" % getenv("HOME"))
-root.title("Pck3r Installer")
+icon = Image.open('%s/.pck3r/icon/pck3r-logo.png' % getenv('HOME'))
+root.title('Pck3r Installer')
 photo = ImageTk.PhotoImage(icon)
 root.wm_iconphoto(False, photo)
-root.geometry("400x80")
-root.configure(background="black")
+root.geometry('400x80')
+root.configure(background='black')
 root.resizable(False, False)
-root.style = Style()
-root.style.theme_use("alt")
-progress.pack(fill="x", pady = 10)
-Button(root, text="install pck3r (system wide) ", command = bar).pack(fill="x", pady = 10)
+progress.pack(fill='x', pady = 10)
+installBtn = tk.Button(root, text='install pck3r (system wide) ', command = bar)
+installBtn.configure(fg='lightblue', bg='darkblue', )
 
-
+installBtn.pack(fill='x', pady = 10)
 
 # infinite loop
-mainloop()
+tk.mainloop()
 
